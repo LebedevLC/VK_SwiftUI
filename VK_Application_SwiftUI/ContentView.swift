c8577a49416135f440f5c72d5d8c120dcbee7dbd
chumovskih_sergey <sergeyka-lalala@yandex.ru>
2022-01-08 14:17:06 +0300

    Initial Commit

---
+++ VK_Application_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VK_Application_SwiftUI
+//
+//  Created by Сергей Чумовских  on 08.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VK_Application_SwiftUI/VK_Application_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_Application_SwiftUIApp.swift
+//  VK_Application_SwiftUI
+//
+//  Created by Сергей Чумовских  on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_Application_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
